c11e83ce741abfd4a3409d57dbd3bdef3f06df15
simbag04 <124486722+simbag04@users.noreply.github.com>
2024-02-15 12:50:07 -0800

    starting point for strategyqa dataset

--- src/strategyqa_dataset.py
+++ src/strategyqa_dataset.py
@@ -15,20 +15,22 @@ class StrategyQADataset(Dataset):
         question = item['question']
         decomposition = item['decomposition']
         evidence = item['evidence']
-        
+
         # Tokenize question and decomposition
-        encoded_inputs = self.tokenizer(question, decomposition, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
-        
+        encoded_inputs = self.tokenizer(
+            question, decomposition, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
+
         # Process evidence
         encoded_evidence = []
         for evidences in evidence:
             encoded_facts = []
             for fact in evidences:
                 if isinstance(fact, list):  # If fact is a list of strings
-                    encoded_fact = self.tokenizer.encode_plus(fact, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
+                    encoded_fact = self.tokenizer.encode_plus(
+                        fact[0], return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
                     encoded_facts.append(encoded_fact)
                 else:  # If fact is a string indicating operation
-                    encoded_facts.append(fact)
+                    encoded_facts.append(fact[0])
             encoded_evidence.append(encoded_facts)
 
         return {
