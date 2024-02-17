5e1af028b9de2a4a111c69d4825e60bb56f62052
simbag04 <124486722+simbag04@users.noreply.github.com>
2024-02-16 21:43:04 -0800

    Get T5 model ready to train Figure out how to convert model output tokens to text

--- src/SQP1Dataset.py
+++ src/SQP1Dataset.py
@@ -1,14 +1,14 @@
 import json
 from torch.utils.data import Dataset
 from SQP1 import SQP1Example
-from transformers import BertTokenizer
+from transformers import AutoTokenizer
 import torch
 from typing import List, Tuple
 from transformers import T5Tokenizer, T5Model
 
 
 class SQP1Dataset(Dataset):
-    tokenizer: T5Tokenizer = None
+    tokenizer: AutoTokenizer = None
 
     def __init__(self, raw_data, tokenizer, max_length=512):
         self.data = [SQP1Example.from_dict(data) for data in raw_data]
@@ -20,86 +20,33 @@ class SQP1Dataset(Dataset):
 
     def __getitem__(self, index):
         return self.data[index]
-        # question = item['question']
-        # decomposition = item['decomposition']
-        # evidence = item['evidence']
-
-        # # Tokenize question and decomposition
-        # encoded_inputs = self.tokenizer(
-        #     question, decomposition, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
-
-        # # Process evidence
-        # encoded_evidence = []
-        # for evidences in evidence:
-        #     encoded_facts = []
-        #     for fact in evidences:
-        #         if isinstance(fact, list):  # If fact is a list of strings
-        #             encoded_fact = self.tokenizer.encode_plus(
-        #                 fact[0], return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
-        #             encoded_facts.append(encoded_fact)
-        #         else:  # If fact is a string indicating operation
-        #             encoded_facts.append(fact[0])
-        #     encoded_evidence.append(encoded_facts)
-
-        # return {
-        #     'input_ids': encoded_inputs['input_ids'],
-        #     'attention_mask': encoded_inputs['attention_mask'],
-        #     'encoded_evidence': encoded_evidence
-        # }
 
     def __iter__(self):
         """
         Get an iterator for the dataset.
         """
-        # TODO: return an iterator for sample_list.
         return iter(self.data)
 
     @staticmethod
-    def collate_fn(samples: List[SQP1Example]) -> dict:
-        input_ids_list = []
-        target_ids_list = []
+    def collate_fn(batched_samples: List[SQP1Example]) -> dict:
+        questions = [sample.question for sample in batched_samples]
+        decomps = ["<SEP>".join(sample.decompositions)
+                   for sample in batched_samples]
 
-        questions = [sample.question for sample in samples]
-        decomps = [sample.decompositions for sample in samples]
+        input_ids = SQP1Dataset.tokenizer(
+            questions, return_tensors="pt", padding=True, truncation=True)
+        target_ids = SQP1Dataset.tokenizer(
+            decomps, return_tensors="pt", padding=True, truncation=True)
 
-        for question, decomposition_questions in zip(questions, decomps):
-            input_ids, target_ids = convert_to_t5_format(
-                SQP1Dataset.tokenizer, question, decomposition_questions)
-            input_ids_list.append(input_ids)
-            target_ids_list.append(target_ids)
-
-        return {'input_ids': input_ids_list, 'target_ids': target_ids_list}
+        return {'input_ids': input_ids, 'target_ids': target_ids}
 
 
 def convert_to_t5_format(tokenizer, question, decomposition_questions):
     # Tokenize the input question and decomposition questions
-    tokenized_question = tokenizer.tokenize(question)
-    tokenized_decomposition_questions = [
-        tokenizer.tokenize(q) for q in decomposition_questions]
+    inputs = tokenizer(question)
+    outputs = tokenizer("<SEP>".join(decomposition_questions))
 
-    # Define special tokens
-    bos_token = tokenizer.bos_token
-    eos_token = tokenizer.eos_token
-    pad_token_id = '<PAD>'
-
-    # Concatenate input and output sequences
-    inputs = [bos_token] + tokenized_question + [eos_token]
-    targets = [[bos_token] + q + [eos_token]
-               for q in tokenized_decomposition_questions]
-
-    # Calculate max length
-    max_length = max(len(inputs), max(len(target) for target in targets))
-
-    # Pad sequences to the same length
-    inputs = inputs + [pad_token_id] * (max_length - len(inputs))
-    targets = [target + [pad_token_id] * (max_length - len(target)) for target in targets]
-
-    # Convert tokens to token IDs
-    input_ids = tokenizer.convert_tokens_to_ids(inputs)
-    target_ids = [tokenizer.convert_tokens_to_ids(
-        target) for target in targets]
-
-    return input_ids, target_ids
+    return inputs, outputs
 
 
 def initialize_datasets(json_file_train, json_file_dev, tokenizer) -> dict:
